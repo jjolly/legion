5248fd892a2a6af6fb1bd22e43a0d2e3d4fae4e2
John Jolly <john.jolly@gmail.com>
2017-09-19 21:28:04 -0600

    Prepared index_tasks.cc for presentation

--- tutorial/02_index_tasks/index_tasks.cc
+++ tutorial/02_index_tasks/index_tasks.cc
@@ -41,94 +41,41 @@ void top_level_task(const Task *task,
                     Context ctx, Runtime *runtime)
 {
   int num_points = 4;
-  // See how many points to run
-  const InputArgs &command_args = Runtime::get_input_args();
-  for (int i = 1; i < command_args.argc; i++) {
-    if (command_args.argv[i][0] == '-') {
-      i++;
-      continue;
-    }
-
-    num_points = atoi(command_args.argv[i]);
-    assert(num_points > 0);
-    break;
-  }
   printf("Running hello world redux for %d points...\n", num_points);
 
-  // To aid in describing structured data, Legion supports
-  // a Rect type which is used to describe an array of
-  // points.  Rects are templated on the number of 
-  // dimensions that they describe.  To specify a Rect
-  // a user gives two Points which specify the lower and
-  // upper bounds on each dimension respectively.  Similar
-  // to the Rect type, a Point type is templated on the
-  // dimensions accessed.  Here we create a 1-D Rect which
-  // we'll use to launch an array of tasks.  Note that the
-  // bounds on Rects are inclusive.
+  // Index space bounds are represented by a Rect object and
+  // is defined by Point object, inclusively
   Rect<1> launch_bounds(Point<1>(0),Point<1>(num_points-1));
-  // Rects can be converted to Domains.  Domains are useful
-  // type which is equivalent to Rects but are not templated
-  // on the number of dimensions.  Users can easily convert
-  // between Domains and Rects using the 'from_rect' and
-  // 'get_rect' methods.  Most Legion runtime calls will 
-  // take Domains, but it often helps in application code
-  // to have type checking support on the number of dimensions.
+  // Domain object is a repreentation of a specific range
   Domain launch_domain = Domain::from_rect<1>(launch_bounds);
 
-  // When we go to launch a large group of tasks in a single
-  // call, we may want to pass different arguments to each
-  // task.  ArgumentMaps allow the user to pass different
-  // arguments to different points.  Note that ArgumentMaps
-  // do not need to specify arguments for all points.  Legion
-  // is intelligent about only passing arguments to the tasks
-  // that have them.  Here we pass some values that we'll
-  // use to illustrate how values get returned from an index
-  // task launch.
+  // An ArgumentMap defines the inputs for a set of tasks
   ArgumentMap arg_map;
   for (int i = 0; i < num_points; i++)
   {
     int input = i + 10;
+    // Domain objects are generic representations of specific dimensions
     arg_map.set_point(DomainPoint::from_point<1>(Point<1>(i)),
         TaskArgument(&input,sizeof(input)));
   }
-  // Legion supports launching an array of tasks with a 
-  // single call.  We call these index tasks as we are launching
-  // an array of tasks with one task for each point in the
-  // array.  Index tasks are launched similar to single
-  // tasks by using an index task launcher.  IndexLauncher
-  // objects take the additional arguments of an ArgumentMap,
-  // a TaskArgument which is a global argument that will
-  // be passed to all tasks launched, and a domain describing
-  // the points to be launched.
+  // IndexLauncher prepares several targets to be launches
   IndexLauncher index_launcher(INDEX_SPACE_TASK_ID,
                                launch_domain,
                                TaskArgument(NULL, 0),
                                arg_map);
-  // Index tasks are launched the same as single tasks, but
-  // return a future map which will store a future for all
-  // points in the index space task launch.  Application
-  // tasks can either wait on the future map for all tasks
-  // in the index space to finish, or it can pull out 
-  // individual futures for specific points on which to wait.
+  // The FutureMap is a collection of results from tasks
   FutureMap fm = runtime->execute_index_space(ctx, index_launcher);
-  // Here we wait for all the futures to be ready
+
+  // Now the top-level tasks waits for the sub-tasks
   fm.wait_all_results();
-  // Now we can check that the future results that came back
-  // from all the points in the index task are double 
-  // their input.
-  bool all_passed = true;
+
   for (int i = 0; i < num_points; i++)
   {
     int expected = 2*(i+10);
+    // And gets the results from each sub-task
     int received = fm.get_result<int>(DomainPoint::from_point<1>(Point<1>(i)));
-    if (expected != received)
-    {
-      printf("Check failed for point %d: %d != %d\n", i, expected, received);
-      all_passed = false;
-    }
+    printf("Task #%d: Expected: %d, Received: %d\n", i, expected, received);
   }
-  if (all_passed)
-    printf("All checks passed!\n");
 }
 
 int index_space_task(const Task *task,
